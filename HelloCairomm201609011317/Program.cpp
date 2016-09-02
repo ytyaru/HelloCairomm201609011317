5a5a59d9549ac19d2b95a0d3181118beaf59d8b3
ytyaru <ytyaru@outlook.jp>
2016-09-03 07:44:56 +0900

    initial commit

---
+++ HelloCairomm201609011317/Program.cpp
@@ -0,0 +1,83 @@
+﻿#include <gtkmm.h>
+#include <glibmm.h>
+#include <string>
+class MyDrawArea : public Gtk::DrawingArea
+{
+public:
+    MyDrawArea(){}
+protected:
+    virtual bool on_expose_event( GdkEventExpose* e );
+};
+bool MyDrawArea::on_expose_event( GdkEventExpose* event )
+{
+	int width = get_width();
+	int height =get_height();
+	double y = 10;
+
+	Cairo::RefPtr< Cairo::Context > cairoctx = get_window()->create_cairo_context();
+
+	// 解像度取得
+	double resolution = get_pango_context()->get_resolution();
+
+	// 背景の塗りつぶし
+	Gdk::Cairo::set_source_color( cairoctx, Gdk::Color( "black" ) ); 
+	cairoctx->paint();
+
+	std::string str = "あいうえお";
+	Glib::ustring text = Glib::locale_to_utf8(str);
+	
+	cairoctx->select_font_face( "Serif", Cairo::FONT_SLANT_NORMAL, Cairo::FONT_WEIGHT_NORMAL );
+	cairoctx->set_font_size( 20 * resolution / 72 );  // pixel = pt * DPI/72
+
+	// 色指定
+	Gdk::Cairo::set_source_color( cairoctx, Gdk::Color( "white" ) ); 
+
+	// extents取得
+	Cairo::TextExtents txext;
+	cairoctx->get_text_extents( text, txext );
+
+	// 描画(センタリング)
+	cairoctx->move_to( width/2 - txext.width/2 - txext.x_bearing, y - txext.y_bearing );
+	cairoctx->show_text( text );
+	
+	// 次の文字列のy座標
+	y += txext.height + 10;
+	
+	str = "かきくけこ";
+	text = Glib::locale_to_utf8(str);
+
+	// フォント指定
+	std::string fontName = "Monospace";
+	Glib::ustring uFontName = Glib::locale_to_utf8(fontName);
+	cairoctx->select_font_face( uFontName, Cairo::FONT_SLANT_NORMAL, Cairo::FONT_WEIGHT_NORMAL );
+	cairoctx->set_font_size( 24 * resolution / 72 );
+    
+	// 色指定
+	Gdk::Cairo::set_source_color( cairoctx, Gdk::Color( "red" ) ); 
+
+	// extents取得
+	cairoctx->get_text_extents( text, txext );
+
+	// 描画(センタリング)
+	cairoctx->move_to( width/2 - txext.width/2 - txext.x_bearing, y - txext.y_bearing );
+	cairoctx->show_text( text );
+
+	return true;
+}
+class MainWin : public Gtk::Window
+{
+    MyDrawArea m_drawarea;
+public:
+    MainWin(){
+        add( m_drawarea );
+        show_all_children();
+    }
+};
+int main( int argc, char *argv[] )
+{
+    Gtk::Main kit( argc, argv );
+    MainWin mainwin;
+    Gtk::Main::run( mainwin );
+
+    return 0;
+}
---
+++ ReadMe.md
@@ -0,0 +1,30 @@
+﻿# このソフトウェアについて #
+
+HelloCairomm201609011317は私個人が学習目的で作成したソフトウェアです。
+
+cairommを使って日本語を描画しました。
+しかし、なぜかフォントの上下左右が反転してしまいました。原因不明。
+
+# 開発環境 #
+
+* Windows XP Pro SP3 32bit
+* VC++ 2010 Express
+    * C++/Win32
+
+## ライブラリ ##
+
+* [Gtkmm 2.22](http://ftp.gnome.org/pub/GNOME/binaries/win32/gtkmm/2.22/gtkmm-win32-devel-2.22.0-2.exe)
+    * GTKmm 2.4
+        * GTK+ 2.22.0
+    * cairomm 1.0
+        * cairo 1.10.0
+
+# イメージ #
+
+![Window](https://cdn-ak.f.st-hatena.com/images/fotolife/y/ytyaru/20160902/20160902145312.png)
+
+# ライセンス #
+
+このソフトウェアはCC0ライセンスです。
+
+[![CC0](http://i.creativecommons.org/p/zero/1.0/88x31.png "CC0")](http://creativecommons.org/publicdomain/zero/1.0/deed.ja)
